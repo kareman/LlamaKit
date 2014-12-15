28968cde4db23f7ff82ec9958baff3e47ac62b73
Kare Morstol <kare@nottoobadsoftware.com>
2014-12-16 00:42:25 +0100

    Make unit tests use custom error type.

--- LlamaKitTests/ResultTests.swift
+++ LlamaKitTests/ResultTests.swift
@@ -9,104 +9,128 @@
 import LlamaKit
 import XCTest
 
+struct TestError: Printable, Equatable {
+  let description: String
+
+  init (_ description: String) {
+    self.description = description
+  }
+}
+
+func == (left: TestError, right: TestError) -> Bool {
+   return left.description == right.description
+}
+
+
 class ResultTests: XCTestCase {
-  let err = NSError(domain: "", code: 11, userInfo: nil)
-  let err2 = NSError(domain: "", code: 12, userInfo: nil)
+  let err = TestError("err")
+  let err2 = TestError("err2")
+
 
   func testSuccessIsSuccess() {
-    let s: Result<Int,NSError> = success(42)
+    let s: Result<Int,TestError> = success(42)
     XCTAssertTrue(s.isSuccess)
   }
 
   func testSuccessReturnsValue() {
-    let s: Result<Int,NSError> = success(42)
+    let s: Result<Int,TestError> = success(42)
     XCTAssertEqual(s.value!, 42)
   }
 
   func testSuccessReturnsNoError() {
-    let s: Result<Int,NSError> = success(42)
+    let s: Result<Int,TestError> = success(42)
     XCTAssert(s.error == nil)
   }
 
-  func testFailureIsNotSuccess() {
+  func testEmptyFailureIsNotSuccess() {
     let f: Result<Bool, ()> = failure()
     XCTAssertFalse(f.isSuccess)
   }
 
+  func testFailureIsNotSuccess() {
+    let f: Result<Bool, TestError> = failure(self.err)
+    XCTAssertFalse(f.isSuccess)
+  }
+
   func testFailureReturnsError() {
-    let f: Result<Int, NSError> = failure(self.err)
+    let f: Result<Int, TestError> = failure(self.err)
     XCTAssertEqual(f.error!, self.err)
   }
 
   func testFailureReturnsNoValue() {
-    let f: Result<Int, NSError> = failure(self.err)
+    let f: Result<Int, TestError> = failure(self.err)
     XCTAssertNil(f.value)
   }
 
   func testMapSuccessUnaryOperator() {
-    let x: Result<Int, NSError> = success(42)
+    let x: Result<Int, TestError> = success(42)
     let y = x.map(-)
     XCTAssertEqual(y.value!, -42)
   }
 
   func testMapFailureUnaryOperator() {
-    let x: Result<Int, NSError> = failure(self.err)
+    let x: Result<Int, TestError> = failure(self.err)
     let y = x.map(-)
     XCTAssertNil(y.value)
     XCTAssertEqual(y.error!, self.err)
   }
 
   func testMapSuccessNewType() {
-    let x: Result<String, NSError> = success("abcd")
+    let x: Result<String, TestError> = success("abcd")
     let y = x.map { countElements($0) }
     XCTAssertEqual(y.value!, 4)
   }
 
   func testMapFailureNewType() {
-    let x: Result<String, NSError> = failure(self.err)
+    let x: Result<String, TestError> = failure(self.err)
     let y = x.map { countElements($0) }
     XCTAssertEqual(y.error!, self.err)
   }
 
-  func doubleSuccess(x: Int) -> Result<Int, NSError> {
+  func doubleSuccess(x: Int) -> Result<Int, TestError> {
     return success(x * 2)
   }
 
-  func doubleFailure(x: Int) -> Result<Int, NSError> {
+  func doubleFailure(x: Int) -> Result<Int, TestError> {
     return failure(self.err)
   }
 
   func testFlatMapSuccessSuccess() {
-    let x: Result<Int, NSError> = success(42)
+    let x: Result<Int, TestError> = success(42)
     let y = x.flatMap(doubleSuccess)
     XCTAssertEqual(y.value!, 84)
   }
 
   func testFlatMapSuccessFailure() {
-    let x: Result<Int, NSError> = success(42)
+    let x: Result<Int, TestError> = success(42)
     let y = x.flatMap(doubleFailure)
     XCTAssertEqual(y.error!, self.err)
   }
 
   func testFlatMapFailureSuccess() {
-    let x: Result<Int, NSError> = failure(self.err2)
+    let x: Result<Int, TestError> = failure(self.err2)
     let y = x.flatMap(doubleSuccess)
     XCTAssertEqual(y.error!, self.err2)
   }
 
   func testFlatMapFailureFailure() {
-    let x: Result<Int, NSError> = failure(self.err2)
+    let x: Result<Int, TestError> = failure(self.err2)
     let y = x.flatMap(doubleFailure)
     XCTAssertEqual(y.error!, self.err2)
   }
 
   func testDescriptionSuccess() {
-    let x: Result<Int, NSError> = success(42)
+    let x: Result<Int, TestError> = success(42)
     XCTAssertEqual(x.description, "Success: 42")
   }
 
+  func testDescriptionFailure() {
+    let x: Result<String, TestError> = failure(self.err)
+    XCTAssert(x.description.hasPrefix("Failure: err"))
+  }
+
   func testCoalesceSuccess() {
-    let r: Result<Int, NSError> = success(42)
+    let r: Result<Int, TestError> = success(42)
     let x = r ?? 43
     XCTAssertEqual(x, 42)
   }
